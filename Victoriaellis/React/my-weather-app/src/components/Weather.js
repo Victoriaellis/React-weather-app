ee0b4ddfee30e773e791e17b820d5c2d6beb84e0
Robyn Ellison <robyn.victoria.ellison@gmail.com>
2021-07-16 16:12:52 +0100

    gitignore file created

--- Victoriaellis/React/my-weather-app/src/components/Weather.js
+++ Victoriaellis/React/my-weather-app/src/components/Weather.js
@@ -9,9 +9,10 @@ export default function weather({weatherData}) {
     <div>
       <h1>THE WEATHER WHERE YOU ARE</h1>
        <h1>{weatherData.name} <i>({weatherData.sys.country})</i></h1>
-       <h2>Today's weather is {weatherData.weather[0].main}</h2>
-       <h3>It is {Math.round(weatherData.main.temp)} °C but feels like {Math.round(weatherData.main.feels_like)}°C</h3>
        <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt="Weather icon"></img>
+       <h2>Outlook: {weatherData.weather[0].main} - {weatherData.weather[0].description}</h2>
+       <h3>It is {Math.round(weatherData.main.temp)} °C</h3>
+       <p><i>feels like {Math.round(weatherData.main.feels_like)}°C</i></p>
     </div>
 )}
 
